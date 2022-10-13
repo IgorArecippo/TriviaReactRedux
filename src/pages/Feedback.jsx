82ddc1ead71fbad62ac8759dbf027f527517cc4d
André Gross <andgross@gmail.com>
2022-10-13 15:39:25 -0300

    finaliza requisito 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   messageFeedback = () => {
     const { assertions } = this.props;
     const assertionsMax = 3;
-    { assertions < assertionsMax ? 'Could be better...' : 'Well Done!'; }
+    return assertions < assertionsMax ? 'Could be better...' : 'Well Done!';
   };
 
   render() {
     const { assertions, score } = this.props;
     return (
-      <div data-testid="feedback-text">
-        <h2 data-testid="feedback-text">{this.messageFeedback()}</h2>
-        <p>Respostas corretas:</p>
-        <p data-testid="feedback-total-question">{assertions}</p>
-        <p>Placar final:</p>
-        <p data-testid="feedbacfeedback-total-score">{score}</p>
+      <div>
+        <Header />
+        <div data-testid="feedback-text">
+          <h2 data-testid="feedback-text">{this.messageFeedback()}</h2>
+          <p>Respostas corretas:</p>
+          <p data-testid="feedback-total-question">{assertions}</p>
+          <p>Placar final:</p>
+          <p data-testid="feedbacfeedback-total-score">{score}</p>
+        </div>
       </div>
     );
   }
